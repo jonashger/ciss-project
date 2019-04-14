8b6058be476509f441afb42dd4890c5d790f7daa
Jonas Halmenschlager <jonassjo@outlook.com>
2019-04-14 20:06:00 -0300

    initial commit

---
+++ ciss-server/ciss-server/src/main/java/br/com/hger/cissserver/CissServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.hger.cissserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CissServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CissServerApplication.class, args);
+	}
+
+}
---
+++ ciss-server/ciss-server/src/test/java/br/com/hger/cissserver/CissServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.hger.cissserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CissServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
